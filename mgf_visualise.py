7dd3eb4868de0d87a92740d49eab9336f0745699
Chris Schudoma <cschu1981@gmail.com>
2015-05-29 18:42:01 +0100

    Switched to scatterplots, added maxMS2spectra-filter

--- mgf_visualise.py
+++ mgf_visualise.py
@@ -25,6 +25,9 @@ def precChargeBinFunc(spectrum, params={}):
 def precIntensityBinFunc(spectrum, params={}):
     intensity = spectrum['params']['pepmass'][1]
     return int(math.log(intensity, 10)) if intensity > 0 else None
+def maxMS2IntensityBinFunc(spectrum, params={}):
+    maxIntensity = max(spectrum['intensity array'])
+    return int(math.log(maxIntensity, 10)) if maxIntensity > 0 else -1
 
 def binSpectra(fi, binfunc, binparams):
     bins = {}
@@ -44,8 +47,26 @@ def binSpectra(fi, binfunc, binparams):
         bins[bin_].insert(1, [])
     return bins
 
+
+def getBinMembers(fi, binfunc, binparams):
+    bins = {}
+    with mgf.read(fi) as reader:
+        for spectrum in reader:
+            pmass = spectrum['params']['pepmass'][0]
+            bin_ = binfunc(spectrum, binparams)
+            if bin_ not in bins:
+                # raw_intensities, raw_masses, norm_masses
+                bins[bin_] = []
+            bins[bin_].append(spectrum['params']['scans'])
+
+    return bins
+
+
+
 def makePlots(fi1, fi2, figfn, binfunc, binparams):
-    bins1, bins2 = map(lambda x:binSpectra(x, binfunc, binparams), [fi1, fi2])
+    #bins1, bins2 = map(lambda x:binSpectra(x, binfunc, binparams), [fi1, fi2])
+    bins1 = binSpectra(fi1, binfunc, binparams)
+    bins2 = binSpectra(fi2, binfunc, binparams)
     for bin_ in sorted(set(bins1).intersection(set(bins2))):
         if bin_ is None:
             continue
@@ -64,13 +85,21 @@ def makePlots(fi1, fi2, figfn, binfunc, binparams):
             title1 = '%s\nprecIntensity=E%02i' % (name1, bin_)
             title2 = '%s\nprecIntensity=E%02i' % (name2, bin_)
             figname = '%s_E%02i.png' % (figfn, bin_)
+        elif figfn == "maxms2intensity":
+            title1 = '%s\nmaxMS2Intensity=E%02i' % (name1, bin_)
+            title2 = '%s\nmaxMS2Intensity=E%02i' % (name2, bin_)
+            figname = '%s_E%02i.png' % (figfn, bin_)
 
         fig = plt.figure(figsize=(10.0, 3.0))
         plot1, plot2 = fig.add_subplot(1, 2, 1), fig.add_subplot(1, 2, 2)
         plot1.set_title(title1)
         plot2.set_title(title2)
-        plot1.plot(bins1[bin_][2], bins1[bin_][0])
-        plot2.plot(bins2[bin_][2], bins2[bin_][0])
+
+        plot1.scatter(bins1[bin_][2], bins1[bin_][0], s=1)
+        plot2.scatter(bins2[bin_][2], bins2[bin_][0], s=1)
+
+        #plot1.plot(bins1[bin_][2], bins1[bin_][0])
+        #plot2.plot(bins2[bin_][2], bins2[bin_][0])
         # CAREFUL: barplots are memory hogs and crash!!!
         #plot1.bar(bins1[bin_][2], bins1[bin_][0], width=0.1, linewidth=2, edgecolor='black')
         #plot2.bar(bins2[bin_][2], bins2[bin_][0], width=0.1, linewidth=2, edgecolor='black')
@@ -79,9 +108,32 @@ def makePlots(fi1, fi2, figfn, binfunc, binparams):
     pass
 
 
-makePlots(sys.argv[1], sys.argv[2], 'pmass', precMassBinFunc, {'winsize': 200})
-makePlots(sys.argv[1], sys.argv[2], 'pcharge', precChargeBinFunc, None)
-makePlots(sys.argv[1], sys.argv[2], 'pintensity', precIntensityBinFunc, None)
+
+def writeBinMembers(fi, binfunc, binparams):
+    members = getBinMembers(fi, binfunc, binparams)
+    with open(os.path.basename(fi) + '.bmembers.txt', 'wb') as fo:
+        for bin_ in sorted(members):
+            fo.write('BIN %s\n' % bin_)
+            for member in members[bin_]:
+                fo.write('%s\n' % member)
+
+
+
+
+def main():
+    # uncomment to generate plots
+    # makePlots(sys.argv[1], sys.argv[2], 'pmass', precMassBinFunc, {'winsize': 200})
+    # makePlots(sys.argv[1], sys.argv[2], 'pcharge', precChargeBinFunc, None)
+    # makePlots(sys.argv[1], sys.argv[2], 'pintensity', precIntensityBinFunc, None)
+    # makePlots(sys.argv[1], sys.argv[2], 'maxms2intensity', maxMS2IntensityBinFunc, None)
+
+    # uncomment to extract bin members
+    # writeBinMembers(sys.argv[1], maxMS2IntensityBinFunc, None)
+    # writeBinMembers(sys.argv[2], maxMS2IntensityBinFunc, None)
+    pass
+
+
+main()
 
 
 
