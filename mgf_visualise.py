66c0f583ea7e1c253092e4f4bd3f0ff31b9905fb
Chris Schudoma <cschu1981@gmail.com>
2015-05-29 10:37:56 +0100

    Initial commit.

---
+++ mgf_visualise.py
@@ -0,0 +1,135 @@
+import sys
+import os
+import math
+
+from pyteomics import mgf
+import matplotlib.pyplot as plt
+import numpy as np
+import pylab
+
+
+
+
+
+plt.figure(figsize=(10.0, 3.0))
+
+raw = plt.subplot(1, 2, 1)
+#raw.ylabel('raw')
+# plt.ylabel('average')
+# plt.plot(data.mean(axis=0))
+
+norm = plt.subplot(1, 2, 2)
+#norm.ylabel('normalised')
+#plt.plot(data.max(axis=0))
+
+def binSpectraByPrecursorMass(fi, winsize=200):
+    bins = {}
+    with mgf.read(fi) as reader:
+        for spectrum in reader:
+            pmass = spectrum['params']['pepmass'][0]
+            bin_ = int(pmass // winsize)
+            if bin_ not in bins:
+                # raw_intensities, raw_masses, norm_masses
+                bins[bin_] = [[], [], []]
+            bins[bin_][0].extend(spectrum['intensity array'])
+            # bins[bin_][1].extend(spectrum['m/z array'])
+            bins[bin_][2].extend(spectrum['m/z array'] - pmass)
+
+    for bin_ in bins:
+        bins[bin_] = list(reversed(map(list, zip(*sorted(zip(bins[bin_][2], bins[bin_][0]))))))
+        bins[bin_].insert(1, [])
+    return bins
+
+def binSpectraByCharge(fi):
+    bins = {}
+    with mgf.read(fi) as reader:
+        for spectrum in reader:
+            pmass = spectrum['params']['pepmass'][0]
+            bin_ = spectrum['params']['charge'][0]
+            if bin_ not in bins:
+                bins[bin_] = [[], [], []]
+            bins[bin_][0].extend(spectrum['intensity array'])
+            # bins[bin_][1].extend(spectrum['m/z array'])
+            bins[bin_][2].extend(spectrum['m/z array'] - pmass)
+    for bin_ in bins:
+        bins[bin_] = list(reversed(map(list, zip(*sorted(zip(bins[bin_][2], bins[bin_][0]))))))
+        bins[bin_].insert(1, [])
+    return bins
+
+
+def precMassBinFunc(spectrum, params={'winsize': 200}):
+    return int(spectrum['params']['pepmass'][0] // params['winsize'])
+def precChargeBinFunc(spectrum, params={}):
+    return spectrum['params']['charge'][0]
+def precIntensityBinFunc(spectrum, params={}):
+    intensity = spectrum['params']['pepmass'][1]
+    return int(math.log(intensity, 10)) if intensity > 0 else None
+
+def binSpectra(fi, binfunc, binparams):
+    bins = {}
+    with mgf.read(fi) as reader:
+        for spectrum in reader:
+            pmass = spectrum['params']['pepmass'][0]
+            bin_ = binfunc(spectrum, binparams)
+            if bin_ not in bins:
+                # raw_intensities, raw_masses, norm_masses
+                bins[bin_] = [[], [], []]
+            bins[bin_][0].extend(spectrum['intensity array'])
+            # bins[bin_][1].extend(spectrum['m/z array'])
+            bins[bin_][2].extend(spectrum['m/z array'] - pmass)
+
+    for bin_ in bins:
+        bins[bin_] = list(reversed(map(list, zip(*sorted(zip(bins[bin_][2], bins[bin_][0]))))))
+        bins[bin_].insert(1, [])
+    return bins
+
+def makePlots(fi1, fi2, figfn, winsize=200):
+    bins1, bins2 = map(lambda x:binSpectraByPrecursorMass(x), [fi1, fi2])
+    for bin_ in sorted(set(bins1).intersection(set(bins2))):
+        interval = (winsize * bin_, winsize * (bin_ + 1))
+        fig = plt.figure(figsize=(10.0, 3.0))
+        plot1, plot2 = fig.add_subplot(1, 2, 1), fig.add_subplot(1, 2, 2)
+        plot1.set_title('%s\n%i - %i D' % ((os.path.basename(fi1),) + interval))
+        plot2.set_title('%s\n%i - %i D' % ((os.path.basename(fi2),) + interval))
+        plot1.plot(bins1[bin_][2], bins1[bin_][0])
+        plot2.plot(bins2[bin_][2], bins2[bin_][0])
+        #plot1.bar(bins1[bin_][2], bins1[bin_][0], width=0.1, linewidth=2, edgecolor='black')
+        #plot2.bar(bins2[bin_][2], bins2[bin_][0], width=0.1, linewidth=2, edgecolor='black')
+        fig.tight_layout()
+        fig.savefig('%s_%i.png' % (figfn, interval[1]), dpi=300)
+    pass
+
+
+#bins1 = binSpectraByPrecursorMass(sys.argv[1])
+#bins2 = binSpectraByPrecursorMass(sys.argv[2])
+makePlots(sys.argv[1], sys.argv[2], 'pmass')
+
+
+
+
+
+
+"""
+fi = '/mnt/slproj/MS_results_temp/Christian/HeLa_150506_20ng_r1.mgf'
+
+raw_intensities = []
+raw_masses, norm_masses = [], []
+
+#def filterSpectrum(spectrum, f):
+#    return spectrum if f(spectrum) else None
+
+with mgf.read(fi) as reader:
+    for spectrum in reader:
+        print spectrum['params']['pepmass']
+        raw_intensities.extend(spectrum['intensity array'])
+        raw_masses.extend(spectrum['m/z array'])
+        norm_masses.extend(spectrum['m/z array'] - spectrum['params']['pepmass'][0])
+    print 'Done.'
+
+raw.plot(raw_masses, raw_intensities)
+norm.plot(norm_masses, raw_intensities)
+
+plt.tight_layout()
+plt.savefig(fi.replace('.mgf', '.png'), dpi=300)
+# plt.show()
+"""
