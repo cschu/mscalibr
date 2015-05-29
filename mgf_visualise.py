50b85c90c187fbf66364ab8ce1d4b03bbc42dd0a
Chris Schudoma <cschu1981@gmail.com>
2015-05-29 11:33:31 +0100

    Cleanup

--- mgf_visualise.py
+++ mgf_visualise.py
@@ -7,10 +7,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pylab
 
-
-
-
-
 plt.figure(figsize=(10.0, 3.0))
 
 raw = plt.subplot(1, 2, 1)
@@ -21,41 +17,6 @@ raw = plt.subplot(1, 2, 1)
 norm = plt.subplot(1, 2, 2)
 #norm.ylabel('normalised')
 #plt.plot(data.max(axis=0))
-
-def binSpectraByPrecursorMass(fi, winsize=200):
-    bins = {}
-    with mgf.read(fi) as reader:
-        for spectrum in reader:
-            pmass = spectrum['params']['pepmass'][0]
-            bin_ = int(pmass // winsize)
-            if bin_ not in bins:
-                # raw_intensities, raw_masses, norm_masses
-                bins[bin_] = [[], [], []]
-            bins[bin_][0].extend(spectrum['intensity array'])
-            # bins[bin_][1].extend(spectrum['m/z array'])
-            bins[bin_][2].extend(spectrum['m/z array'] - pmass)
-
-    for bin_ in bins:
-        bins[bin_] = list(reversed(map(list, zip(*sorted(zip(bins[bin_][2], bins[bin_][0]))))))
-        bins[bin_].insert(1, [])
-    return bins
-
-def binSpectraByCharge(fi):
-    bins = {}
-    with mgf.read(fi) as reader:
-        for spectrum in reader:
-            pmass = spectrum['params']['pepmass'][0]
-            bin_ = spectrum['params']['charge'][0]
-            if bin_ not in bins:
-                bins[bin_] = [[], [], []]
-            bins[bin_][0].extend(spectrum['intensity array'])
-            # bins[bin_][1].extend(spectrum['m/z array'])
-            bins[bin_][2].extend(spectrum['m/z array'] - pmass)
-    for bin_ in bins:
-        bins[bin_] = list(reversed(map(list, zip(*sorted(zip(bins[bin_][2], bins[bin_][0]))))))
-        bins[bin_].insert(1, [])
-    return bins
-
 
 def precMassBinFunc(spectrum, params={'winsize': 200}):
     return int(spectrum['params']['pepmass'][0] // params['winsize'])
@@ -83,26 +44,45 @@ def binSpectra(fi, binfunc, binparams):
         bins[bin_].insert(1, [])
     return bins
 
-def makePlots(fi1, fi2, figfn, winsize=200):
-    bins1, bins2 = map(lambda x:binSpectraByPrecursorMass(x), [fi1, fi2])
+def makePlots(fi1, fi2, figfn, binfunc, binparams):
+    bins1, bins2 = map(lambda x:binSpectra(x, binfunc, binparams), [fi1, fi2])
     for bin_ in sorted(set(bins1).intersection(set(bins2))):
-        interval = (winsize * bin_, winsize * (bin_ + 1))
+        if bin_ is None:
+            continue
+        name1, name2 = map(os.path.basename, [fi1, fi2])
+        if figfn == "pmass":
+            winsize = binparams['winsize']
+            interval = (winsize * bin_, winsize * (bin_ + 1))
+            title1 = '%s\n%i-%iD' % ((name1,) + interval)
+            title2 = '%s\n%i-%iD' % ((name2,) + interval)
+            figname = '%s_%i-%i.png' % ((figfn,) + interval)
+        elif figfn == "pcharge":
+            title1 = '%s\ncharge=%i+' % (name1, bin_)
+            title2 = '%s\ncharge=%i+' % (name2, bin_)
+            figname = '%s_%i+.png' % (figfn, bin_)
+        elif figfn == "pintensity":
+            title1 = '%s\nprecIntensity=E%02i' % (name1, bin_)
+            title2 = '%s\nprecIntensity=E%02i' % (name2, bin_)
+            figname = '%s_E%02i.png' % (figfn, bin_)
+
         fig = plt.figure(figsize=(10.0, 3.0))
         plot1, plot2 = fig.add_subplot(1, 2, 1), fig.add_subplot(1, 2, 2)
-        plot1.set_title('%s\n%i - %i D' % ((os.path.basename(fi1),) + interval))
-        plot2.set_title('%s\n%i - %i D' % ((os.path.basename(fi2),) + interval))
+        plot1.set_title(title1)
+        plot2.set_title(title2)
         plot1.plot(bins1[bin_][2], bins1[bin_][0])
         plot2.plot(bins2[bin_][2], bins2[bin_][0])
+        # CAREFUL: barplots are memory hogs and crash!!!
         #plot1.bar(bins1[bin_][2], bins1[bin_][0], width=0.1, linewidth=2, edgecolor='black')
         #plot2.bar(bins2[bin_][2], bins2[bin_][0], width=0.1, linewidth=2, edgecolor='black')
         fig.tight_layout()
-        fig.savefig('%s_%i.png' % (figfn, interval[1]), dpi=300)
+        fig.savefig(figname, dpi=300)
     pass
 
 
-#bins1 = binSpectraByPrecursorMass(sys.argv[1])
-#bins2 = binSpectraByPrecursorMass(sys.argv[2])
-makePlots(sys.argv[1], sys.argv[2], 'pmass')
+makePlots(sys.argv[1], sys.argv[2], 'pmass', precMassBinFunc, {'winsize': 200})
+makePlots(sys.argv[1], sys.argv[2], 'pcharge', precChargeBinFunc, None)
+makePlots(sys.argv[1], sys.argv[2], 'pintensity', precIntensityBinFunc, None)
+
 
 
 
@@ -114,9 +94,6 @@ fi = '/mnt/slproj/MS_results_temp/Christian/HeLa_150506_20ng_r1.mgf'
 
 raw_intensities = []
 raw_masses, norm_masses = [], []
-
-#def filterSpectrum(spectrum, f):
-#    return spectrum if f(spectrum) else None
 
 with mgf.read(fi) as reader:
     for spectrum in reader:
@@ -131,5 +108,4 @@ norm.plot(norm_masses, raw_intensities)
 
 plt.tight_layout()
 plt.savefig(fi.replace('.mgf', '.png'), dpi=300)
-# plt.show()
 """
