77a11a7d52c5a6db5d691b57473688170fe781dc
Chris Schudoma <cschu1981@gmail.com>
2015-06-09 13:16:02 +0100

    Figures are now closed after saving.

--- mgf_visualise.py
+++ mgf_visualise.py
@@ -60,43 +60,48 @@ def makePlot(figure, where, title, x, y):
     pl.scatter(x, y, s=1)
     return pl
 
-def makePlots(fi1, fi2, figfn, binfunc, binparams):
+def getPlotLabels(fn, ptype, bin_, binparams):
+    name = os.path.basename(fn)
+    if ptype == "pmass":
+        winsize = binparams['winsize']
+        interval = (winsize * bin_, winsize * (bin_ + 1))
+        title = '%s\n%i-%iD' % ((name,) + interval)
+        figname = '%s_%i-%i.png' % ((ptype,) + interval)
+    elif ptype == "pcharge":
+        title = '%s\ncharge=%i+' % (name, bin_)
+        figname = '%s_%i+.png' % (ptype, bin_)
+    elif ptype == "pintensity":
+        title = '%s\nprecIntensity=E%02i' % (name, bin_)
+        figname = '%s_E%02i.png' % (ptype, bin_)
+    elif ptype == "maxms2intensity":
+        title = '%s\nmaxMS2Intensity=E%02i' % (name, bin_)
+        figname = '%s_E%02i.png' % (ptype, bin_)
+    return title, figname
+
+def adjustAxes(plot1, plot2):
+    ax1, ax2 = plot1.axis(), plot2.axis()
+    sharedAxis = min(ax1[0], ax2[0]), max(ax1[1], ax2[1]), min(ax1[2], ax2[2]), max(ax1[3], ax2[3])
+    plot1.axis(sharedAxis)
+    plot2.axis(sharedAxis)
+    pass
+
+def makePlots(fi1, fi2, ptype, binfunc, binparams):
     #bins1, bins2 = map(lambda x:binSpectra(x, binfunc, binparams), [fi1, fi2])
     bins1 = binSpectra(fi1, binfunc, binparams)
     bins2 = binSpectra(fi2, binfunc, binparams)
     for bin_ in sorted(set(bins1).intersection(set(bins2))):
         if bin_ is None:
             continue
-        name1, name2 = map(os.path.basename, [fi1, fi2])
-        if figfn == "pmass":
-            winsize = binparams['winsize']
-            interval = (winsize * bin_, winsize * (bin_ + 1))
-            title1 = '%s\n%i-%iD' % ((name1,) + interval)
-            title2 = '%s\n%i-%iD' % ((name2,) + interval)
-            figname = '%s_%i-%i.png' % ((figfn,) + interval)
-        elif figfn == "pcharge":
-            title1 = '%s\ncharge=%i+' % (name1, bin_)
-            title2 = '%s\ncharge=%i+' % (name2, bin_)
-            figname = '%s_%i+.png' % (figfn, bin_)
-        elif figfn == "pintensity":
-            title1 = '%s\nprecIntensity=E%02i' % (name1, bin_)
-            title2 = '%s\nprecIntensity=E%02i' % (name2, bin_)
-            figname = '%s_E%02i.png' % (figfn, bin_)
-        elif figfn == "maxms2intensity":
-            title1 = '%s\nmaxMS2Intensity=E%02i' % (name1, bin_)
-            title2 = '%s\nmaxMS2Intensity=E%02i' % (name2, bin_)
-            figname = '%s_E%02i.png' % (figfn, bin_)
+        title1, figname = getPlotLabels(fi1, ptype, bin_, binparams)
+        title2, figname = getPlotLabels(fi2, ptype, bin_, binparams)
 
         fig = plt.figure(figsize=(10.0, 3.0))
         plot1, plot2 = makePlot(fig, 121, title1, bins1[bin_][2], bins1[bin_][0]), makePlot(fig, 122, title2, bins2[bin_][2], bins2[bin_][0])
-
-        ax1, ax2 = plot1.axis(), plot2.axis()
-        sharedAxis = min(ax1[0], ax2[0]), max(ax1[1], ax2[1]), min(ax1[2], ax2[2]), max(ax1[3], ax2[3])
-        plot1.axis(sharedAxis)
-        plot2.axis(sharedAxis)
+        adjustAxes(plot1, plot2)
 
         fig.tight_layout()
         fig.savefig(figname, dpi=300)
+        plt.close(fig)
     pass
 
 def writeBinMembers(fi, binfunc, binparams):
